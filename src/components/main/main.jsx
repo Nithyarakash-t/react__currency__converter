42210161f6c02c03e4c31c3ad8d20152d38d0c15
Thiyagarajan, Nithyaprakash <n.d.thiyagarajan@accenture.com>
2023-11-14 15:30:04 +0530

    minor html and scss updates

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,6 @@
 /**
  * TODOS
- * 1. Implement Swap
+ * 1. Implement Swap -- LATER
  * 2. Add a for loop
  * 3. Use real time data
  * 4. Explore the possibility of states for input, select and output
@@ -14,6 +14,7 @@
  * 
  * 7. Implement a form -- not req
  * 8. Two way conversion 
+ * 9. Graph
  */
 
 
@@ -70,10 +71,51 @@ function Main() {
 
     return (
         <>
-            <div className='cc-wrapper'>
-                <div className='cc-header' role='heading' aria-level='1'>Currency Converter</div>
+            <div className='cc-container'>
+                <div className='cc-wrapper'>
+                    <div className='cc-header' role='heading' aria-level='1'>Currency Converter</div>
 
-                <div className="c-cC__wrap">
+                    <div className='cc-body'>
+                        <div className='cc-inpwrap'>
+                            <input type="number" name="input_1" id='input_1' className="cc-input" min="1" placeholder="Number Input..."/>
+                            <select name='cc-select' id='cc-select_1' aria-label='currency 1'>
+                                <option value="">--Please choose an option--</option>
+                                <option value="usd">US Dollar - USD</option>
+                                <option value="inr">Indian Rupee - INR</option>
+                                <option value="eur">Euro - EUR</option>
+                                <option value="gbp">UK Pound - GBP</option>
+                            </select>
+                        </div>
+                        <div className=''>
+                            <button type="button" className="btn btn-secondary" onClick={handleSwap}>Swap</button>
+                        </div>
+                        <div className='cc-inpwrap'>
+                            <select name='cc-select_2' id='cc-select_2' aria-label='currency 2'>
+                                <option value="">--Please choose an option--</option>
+                                <option value="usd">US Dollar - USD</option>
+                                <option value="inr">Indian Rupee - INR</option>
+                                <option value="eur">Euro - EUR</option>
+                                <option value="gbp">UK Pound - GBP</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="cc-footer">
+                        <button type="button" className="btn btn-success" onClick={handleconvert}>Convert</button>
+                        <button type="button" className="btn btn-danger" onClick={handleReset}>Reset</button>
+                    </div>
+
+                    <div className='cc-result' id='output'></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Main;
+
+/**
+ * <div className="c-cC__wrap">
                     <div className="c-cC__input">
                         <div className="c-cC__input-top">
                             <div className="c-cC__input-wrap">
@@ -108,9 +150,4 @@ function Main() {
 
                     <div className='c-cc__result' id="output"></div>
                 </div>
-            </div>
-        </>
-    )
-}
-
-export default Main;
+ */
